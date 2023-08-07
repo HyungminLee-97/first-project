848e259910186d7ab82fb71ac2410fdc5d55b8c5
‘dlgudals1744@nate.com’ <dlgudals1744@nate.com>
2023-08-07 10:50:28 +0900

    part2-2

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 //기본 세팅(expres 라이브러리) - 시작
 const express = require("express");
 const app = express();
+const MongoClient = require("mongodb").MongoClient;
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(8080, function () {
-  console.log("8080포트가 열렸습니다.");
-});
-//기본 세팅(expres 라이브러리, body-parser) - 끝
+// db 선언
+let db;
+
+MongoClient.connect(
+  "mongodb+srv://admin:qwer1234@cluster0.gq7c239.mongodb.net/?retryWrites=true&w=majority",
+  function (err, client) {
+    if (err) return console.log(err);
+
+    // db 저장 코드
+    db = client.db("hm-project");
+    db.collection("post").insertOne(
+      { name: "John", age: 20, _id: 1 },
+      function (err, result) {
+        console.log("저장완료");
+      }
+    );
+
+    app.listen(8080, function () {
+      console.log("8080포트가 열렸습니다.");
+    });
+  }
+);
+//기본 세팅(expres 라이브러리, body-parser, Mongodb) - 끝
 
 // "/pet" 출력
 app.get("/pet", (req, res) => {
@@ -38,3 +58,8 @@ app.post("/add", (req, res) => {
   console.log(req.body.title);
   console.log(req.body.date);
 });
+
+// 어떤 사람이 /add 라는 경로로 post 요청을 하면,
+// 데이터 2개(날짜, 제목)를 보내주는데,
+// 이 때, 'post'라는 이름을 가진 collection에 두 개 데이터를 저장하기
+// {제목: "asdf", 날짜 : "asdf"}
