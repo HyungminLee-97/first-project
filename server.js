87dfcedac7e71ddd960f8cb2953d38fb7e567a14
‘dlgudals1744@nate.com’ <dlgudals1744@nate.com>
2023-08-07 20:47:13 +0900

    part2-3

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const MongoClient = require("mongodb").MongoClient;
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.set("view engine", "ejs");
 // db 선언
 let db;
 
@@ -13,14 +13,8 @@ MongoClient.connect(
   function (err, client) {
     if (err) return console.log(err);
 
-    // db 저장 코드
+    // db 연결
     db = client.db("hm-project");
-    db.collection("post").insertOne(
-      { name: "John", age: 20, _id: 1 },
-      function (err, result) {
-        console.log("저장완료");
-      }
-    );
 
     app.listen(8080, function () {
       console.log("8080포트가 열렸습니다.");
@@ -52,14 +46,19 @@ app.get("/write", (req, res) => {
 // "/add" 경로로 POST 요청 시,
 app.post("/add", (req, res) => {
   res.send("전송 완료");
-  // 여기서 저장된 데이터는 req애 저장되어 있음
-  // req에 저장된 데이터 꺼내 쓰려면 body-parser 라이브러리 설치 필요
-  console.log(req.body);
   console.log(req.body.title);
   console.log(req.body.date);
+  // 여기서 저장된 데이터는 req애 저장되어 있음
+  // req에 저장된 데이터 꺼내 쓰려면 body-parser 라이브러리 설치 필요
+  db.collection("post").insertOne(
+    { title: req.body.title, date: req.body.date },
+    function (err, result) {
+      console.log("저장완료");
+    }
+  );
 });
 
-// 어떤 사람이 /add 라는 경로로 post 요청을 하면,
-// 데이터 2개(날짜, 제목)를 보내주는데,
-// 이 때, 'post'라는 이름을 가진 collection에 두 개 데이터를 저장하기
-// {제목: "asdf", 날짜 : "asdf"}
+// "/list" 출력
+app.get("/list", (req, res) => {
+  res.render("list.ejs");
+});
